61985391fa7fc938c3e9338cf16513d7358ccda3
Денис Лановенко <denis-lanovenko97@mail.ru>
2024-03-26 09:23:54 +0000

    final

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -6,20 +6,27 @@ def parse_edge(s):
     user, follower = s.split("\t")    
     return (int(user), int(follower))
 
-def step(item):    
-    prev_v, prev_d, next_v = item[0], item[1][0], item[1][1]    
-    prev_d = prev_d if prev_d is not None else []    
+def step(item):
+    prev_v, prev_d, next_v = item[0], item[1][0], item[1][1]        
+    prev_d = prev_d if prev_d is not None and isinstance(prev_d, list) else []       
     return (next_v, prev_d + [next_v]) if len(prev_d + [next_v]) <= max_path_length else (next_v, prev_d)
 
 def complete(item):
     v, old_d, new_d = item[0], item[1][0], item[1][1]
-    if old_d is not None and len(old_d) == 0:
+    
+    old_d = old_d if old_d is not None and isinstance(old_d, list) else []
+    new_d = new_d if new_d is not None and isinstance(new_d, list) else []
+    
+    if len(old_d) == 0 and len(new_d) != 0:
         return (v, new_d)
-    if old_d is not None and new_d is not None:
-        return (v, min(old_d, new_d, key=len))
-    if old_d is None:
-        return (v, new_d)
-    return (v, old_d)
+
+    if len(old_d) != 0 and len(new_d) != 0:
+        min_length = min(len(old_d[0]), len(new_d[0])) if old_d and new_d and isinstance(old_d[0], list) and isinstance(new_d[0], list) else 0
+        all_paths = [path for path in old_d + new_d if isinstance(path, list) and len(path) == min_length]
+
+        return (v, all_paths)
+    
+    return (v, old_d if len(old_d) != 0 else new_d)
 
 
 conf = SparkConf()
@@ -28,6 +35,7 @@ conf.set("spark.ui.port", "4099")
 sc = SparkSession.builder.config(conf=conf).appName("Spark SQL").getOrCreate()
 
 start_node, end_node, max_path_length, input_path, output_path = int(sys.argv[1]), int(sys.argv[2]), 100, sys.argv[3], sys.argv[4]
+
 edges = sc.sparkContext.textFile(input_path).map(parse_edge).cache()
 forward_edges = edges.map(lambda e: (e[1], e[0])).partitionBy(200).persist()
 
@@ -54,16 +62,11 @@ while True:
     distances = new_distances
 
 paths = new_distances.filter(lambda i: i[0] == end_node).collect()
+all_shortest_paths = [str(start_node) + ',' + ','.join(map(str, path[1])) for path in paths if path[1] is not None]
 
 spark = SparkSession(sc)
 
-path_to_save = [str(start_node)] + list(map(str, paths[0][1]))
-
-text_to_save = ','.join(path_to_save)
-
-df = spark.sparkContext.createDataFrame([("1", text_to_save)], ["index", "path"])
-path_to_save
-
+df = spark.sparkContext.createDataFrame([(i + 1, path) for i, path in enumerate(all_shortest_paths)], ["index", "path"])
 df.select("path").write.format("text").option("header", "false").mode('overwrite').save(output_path)
 
 sc.stop()
