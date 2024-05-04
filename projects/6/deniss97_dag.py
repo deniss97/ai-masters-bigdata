e4afd8cc2588e9d00ad28af7d9ce6acc130b7d61
Денис Лановенко <denis-lanovenko97@mail.ru>
2024-05-04 17:34:41 +0000

    fixed

--- projects/6/deniss97_dag.py
+++ projects/6/deniss97_dag.py
@@ -13,6 +13,8 @@ default_args = {
     'retries': 1,
 }
 
+base_dir = '{{ dag_run.conf["base_dir"] if dag_run else "" }}'
+
 dag = DAG(
     'deniss97_dag',
     default_args=default_args,
@@ -26,7 +28,7 @@ start = EmptyOperator(task_id='start', dag=dag)
 
 feature_eng_train_task = SparkSubmitOperator(
     task_id='feature_eng_train_task',
-    application="{{ var.value.base_dir }}/feature_eng.py",
+    application=f"{base_dir}/feature_eng.py",
     name='feature_engineering',
     conn_id='spark_default',
     executor_cores=1,
@@ -44,20 +46,19 @@ feature_eng_train_task = SparkSubmitOperator(
 
 download_train_task = BashOperator(
     task_id='download_train_task',
-    bash_command='hdfs dfs -get /user/deniss97/deniss97_train_out {{ var.value.base_dir }}/deniss97_train_out_local',
+    bash_command=f"hdfs dfs -get /user/deniss97/deniss97_train_out {base_dir}/deniss97_train_out_local",
     dag=dag
 )
 
 train_task = BashOperator(
     task_id='train_task',
-    bash_command='/opt/conda/envs/dsenv/bin/python {{ var.value.base_dir }}/train_model.py --train-in {{ var.value.base_dir }}/deniss97_train_out_local --model-out {{ var.value.base_dir }}/6.joblib',
+    bash_command=f"/opt/conda/envs/dsenv/bin/python {base_dir}/train_model.py --train-in {base_dir}/deniss97_train_out_local --model-out {base_dir}/6.joblib",
     dag=dag
 )
 
-
 model_sensor = FileSensor(
     task_id='model_sensor',
-    filepath='{{ var.value.base_dir }}/6.joblib',
+    filepath=f"{base_dir}/6.joblib",
     poke_interval=300,
     timeout=3600,
     dag=dag
@@ -65,23 +66,24 @@ model_sensor = FileSensor(
 
 feature_eng_test_task = SparkSubmitOperator(
     task_id='feature_eng_test_task',
-    application="{{ var.value.base_dir }}/feature_eng.py",
+    application=f"{base_dir}/feature_eng.py",
     name="feature_eng_test",
     application_args=["--path-in", "/datasets/amazon/amazon_extrasmall_test.json",
-                      "--path-out", "{{ var.value.base_dir }}/deniss97_test_out"],
+                      "--path-out", f"{base_dir}/deniss97_test_out"],
     dag=dag,
 )
 
 predict_task = SparkSubmitOperator(
     task_id='predict_task',
-    application="{{ var.value.base_dir }}/predict.py",
+    application=f"{base_dir}/predict.py",
     name="make_predictions",
-    application_args=["--test-in", "{{ var.value.base_dir }}/deniss97_test_out",
+    application_args=["--test-in", f"{base_dir}/deniss97_test_out",
                       "--pred-out", "/user/deniss97/deniss97_hw6_prediction",
-                      "--sklearn-model-in", "{{ var.value.base_dir }}/6.joblib"],
+                      "--sklearn-model-in", f"{base_dir}/6.joblib"],
     dag=dag,
 )
 
 end = EmptyOperator(task_id='end', dag=dag)
 
 start >> feature_eng_train_task >> download_train_task >> train_task >> model_sensor >> feature_eng_test_task >> predict_task >> end
+
