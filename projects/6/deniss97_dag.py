44fe3c8e33b6e50c8fd598a44fdd9178fda6dff7
Денис Лановенко <denis-lanovenko97@mail.ru>
2024-05-05 16:00:59 +0000

    fixed 3

--- projects/6/deniss97_dag.py
+++ projects/6/deniss97_dag.py
@@ -42,7 +42,8 @@ feature_eng_train_task = SparkSubmitOperator(
     env_vars={
         'PYSPARK_PYTHON': '/opt/conda/envs/dsenv/bin/python'
     },
-    application_args=[],
+    application_args=["--path-in", "/datasets/amazon/amazon_extrasmall_train.json",
+                      "--path-out", f"{base_dir}/deniss97_train_out"],
     packages='org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1',
     driver_memory='1g',
     spark_binary='/usr/bin/spark3-submit',
