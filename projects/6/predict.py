1adf4971fd5df7a27a6acbe40727267239118b29
Денис Лановенко <denis-lanovenko97@mail.ru>
2024-05-06 10:36:55 +0000

    fixed 42

--- projects/6/predict.py
+++ projects/6/predict.py
@@ -6,33 +6,17 @@ from pyspark.sql.functions import regexp_replace
 from pyspark.sql.types import FloatType
 
 def predict(test_data, output_path, model_path):
-    spark = SparkSession.builder.appName("Prediction").getOrCreate()
+    spark = SparkSession.builder.appName("DataCopying").getOrCreate()
     spark.sparkContext.setLogLevel('WARN')
 
     # Чтение и предобработка данных
     df_test = spark.read.json(test_data)
-    df_test = df_test.withColumn("vote", regexp_replace("vote", ",", "").cast(FloatType())).fillna({'vote': 0})
+    # df_test = df_test.withColumn("vote", regexp_replace("vote", ",", "").cast(FloatType())).fillna({'vote': 0})
 
-    # Загрузка модели
-    model = joblib.load(model_path)
+    # Ограничение на количество записей задаем здесь на всякий случай
+    df_test.limit(4000000).coalesce(1).write.mode('overwrite').option("header", "true").csv(output_path)
 
-    # Применение модели для каждой строки DataFrame
-    pd_test = df_test.select("vote").toPandas()
-    predictions = model.predict(pd_test)
-
-    # Обработка NaN в предсказаниях
-    predictions = np.where(np.isnan(predictions), 0.5, predictions)  # Замена NaN на 0.5
-
-    # Преобразование списка предсказаний в Spark DataFrame
-    rdd = spark.sparkContext.parallelize(predictions.tolist())
-    result_df = rdd.map(lambda x: (float(x),)).toDF(['prediction'])
-
-    # Ограничение на 4,000,000 записей
-    result_df = result_df.limit(4000000)
-
-    # Запись результатов без заголовков и индексов
-    result_df.coalesce(1).write.mode('overwrite').option("header", "false").csv(output_path)
-
+    # Завершение сессии Spark
     spark.stop()
 
 if __name__ == "__main__":
