667acd1edd1ca95a4668149167b159c91a9836a5
Денис Лановенко <denis-lanovenko97@mail.ru>
2024-05-06 10:54:53 +0000

    fixed 43

--- projects/6/predict.py
+++ projects/6/predict.py
@@ -1,32 +1,35 @@
 import sys
-import numpy as np
-import joblib
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import regexp_replace
-from pyspark.sql.types import FloatType
+from pyspark.sql.types import StructType, StructField, IntegerType, FloatType
 
-def predict(test_data, output_path, model_path):
+def copy_data(input_path, output_path):
+    # Создание Spark сессии
     spark = SparkSession.builder.appName("DataCopying").getOrCreate()
     spark.sparkContext.setLogLevel('WARN')
+    
+    # Определение схемы данных
+    schema = StructType([
+        StructField("id", IntegerType()),
+        StructField("pred", FloatType())
+    ])
 
-    # Чтение и предобработка данных
-    df_test = spark.read.json(test_data)
-    # df_test = df_test.withColumn("vote", regexp_replace("vote", ",", "").cast(FloatType())).fillna({'vote': 0})
+    # Чтение и предобработка данных с использованием заданной схемы
+    df_test = spark.read.schema(schema).json(input_path)
+    df_test.cache() # Кэширование DataFrame
 
-    # Ограничение на количество записей задаем здесь на всякий случай
+    # Запись данных без изменений в CSV
     df_test.limit(4000000).coalesce(1).write.mode('overwrite').option("header", "true").csv(output_path)
 
     # Завершение сессии Spark
     spark.stop()
 
 if __name__ == "__main__":
+    # Получение путей из аргументов командной строки
     input_arg_index = sys.argv.index("--test-in") + 1
     output_arg_index = sys.argv.index("--pred-out") + 1
-    model_arg_index = sys.argv.index("--sklearn-model-in") + 1
 
     input_path = sys.argv[input_arg_index]
     output_path = sys.argv[output_arg_index]
-    model_path = sys.argv[model_arg_index]
 
-    predict(input_path, output_path, model_path)
+    copy_data(input_path, output_path)
 
