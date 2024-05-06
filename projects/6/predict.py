2c2ae129c9db354e1a7e3421a507a00894395fa6
Денис Лановенко <denis-lanovenko97@mail.ru>
2024-05-06 09:07:31 +0000

    fixed 35

--- projects/6/predict.py
+++ projects/6/predict.py
@@ -1,40 +1,41 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.ml import PipelineModel
-import pandas as pd
 import joblib
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType, FloatType
-import numpy as np
 from pyspark.sql.functions import regexp_replace
+from pyspark.sql.types import FloatType, StructType, StructField, DoubleType
 
 def predict(test_data, output_path, model_path):
     spark = SparkSession.builder.appName("Prediction").getOrCreate()
     spark.sparkContext.setLogLevel('WARN')
 
     df_test = spark.read.json(test_data)
+
+    # Уменьшение запятых и преобразование 'vote' в числа с плавающей точкой
     df_test = df_test.withColumn("vote", regexp_replace("vote", ",", "").cast(FloatType())).fillna({'vote': 0})
-    
+
+    # Загрузка модели
     model = joblib.load(model_path)
-    
+
     # Применение модели для каждой строки DataFrame
     pd_test = df_test.select("vote").toPandas()
     predictions = model.predict(pd_test)
-    
+
+    # Преобразование pandas.DataFrame в Spark DataFrame
+    rdd = spark.sparkContext.parallelize(predictions.tolist())
+    result_df = rdd.map(lambda x: (float(x),)).toDF(['prediction'])
+
     # Запись результатов
-    result_df = spark.createDataFrame(pd.DataFrame(predictions, columns=['prediction']))
     result_df.write.csv(output_path, mode='overwrite', header=True)
 
     spark.stop()
-    
+
 if __name__ == "__main__":
     input_arg_index = sys.argv.index("--test-in") + 1
     output_arg_index = sys.argv.index("--pred-out") + 1
     model_arg_index = sys.argv.index("--sklearn-model-in") + 1
-    
+
     input_path = sys.argv[input_arg_index]
     output_path = sys.argv[output_arg_index]
     model_path = sys.argv[model_arg_index]
 
     predict(input_path, output_path, model_path)
-
